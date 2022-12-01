637ac8e6c381741cddbfc28dfe048b3986c4cdc3
muhajir-artapuri <muhajir@artapuri.co.id>
2022-12-01 11:23:15 +0700

    feat: komik detail, search

--- backend/app.py
+++ backend/app.py
@@ -109,118 +109,6 @@ def home():
     })
 
 
-# url = "https://komikcast.site"
-# home_page = req.get(url)
-
-# soup = BeautifulSoup(home_page.text, "html.parser")
-
-
-# @app.route('/api/hot-komik-update', methods=['GET'])
-# def hot_komik_update():
-#     swiper_wrapper = soup.find_all("div", class_="swiper-wrapper")
-#     hot_komik_update = []
-
-#     for link in swiper_wrapper:
-#         a = link.find_all("a")
-#         for item in a:
-#             url = item.get("href")
-#             class_title = item.find(class_="title")
-#             img = item.find("img")
-#             title = class_title.text
-#             image = img["src"]
-#             rating = item.find(class_='numscore').text
-#             chapter = item.find(class_='chapter').text
-#             hot_komik_update.append(
-#                 {"url": url, "title": title, "image": image, "rating": rating, 'chapter': chapter})
-
-#     return jsonify({
-#         'status': True,
-#         'response': hot_komik_update,
-#     })
-
-
-# @app.route('/api/rilisan-terbaru', methods=['GET'])
-# def rilisan_terbaru():
-#     listupd = soup.find_all("div", class_='listupd')
-#     rilisan_terbaru = []
-
-#     second_index_of_listupd = 0
-#     for utao in listupd:
-#         if second_index_of_listupd == 2:
-#             uta = utao.find_all("div", class_="uta")
-#             for uta_child in uta:
-#                 luf = uta_child.find_all("div", class_="luf")
-#                 imgu = uta_child.find_all('div', class_="imgu data-tooltip")
-#                 for img in imgu:
-#                     image = img.find_all('img')
-#                     for src in image:
-#                         image_komik = src['src']
-#                 for luf_child in luf:
-#                     a_series = luf_child.find_all(
-#                         "a", class_="series data-tooltip")
-#                     for link in a_series:
-#                         url_komik = link.get("href")
-#                         title_komik = link.find("h3").text
-
-#                     ul = luf_child.find_all("ul")
-#                     for li in ul:
-#                         a = li.find_all("a")
-#                         i = li.find_all('i')
-#                         daftar_chapter = []
-#                         updated_at = []
-#                         for italic in i:
-#                             updated_at.append(italic.text)
-#                         updated_at_index = 0
-#                         for link in a:
-#                             url_chapter = link.get("href")
-#                             title_chapter = link.text
-#                             daftar_chapter.append(
-#                                 {"url": url_chapter, "title": title_chapter, "updated_at": updated_at[updated_at_index]})
-#                             updated_at_index += 1
-#                     rilisan_terbaru.append(
-#                         {"url": url_komik, "title": title_komik,  'image': image_komik, "chapter": daftar_chapter})
-#         second_index_of_listupd += 1
-
-#     return jsonify({
-#         'status': True,
-#         'response': rilisan_terbaru,
-#     })
-
-
-# @app.route('/api/popular-manga', methods=['GET'])
-# def popular_manga():
-#     serieslist_pop = soup.find_all("div", class_="serieslist pop")
-#     daftar_popular_manga = []
-
-#     for pop in serieslist_pop:
-#         li = pop.find_all('li')
-#         for list in li:
-#             imgseries = list.find_all('div', class_="imgseries")
-#             for image in imgseries:
-#                 img = image.find_all('img')
-#                 for url in img:
-#                     src = url['src']
-#             leftseries = list.find_all('div', class_="leftseries")
-#             for komik in leftseries:
-#                 h2 = komik.find('h2')
-#                 span = komik.find_all('span')
-#                 genre = []
-#                 for a in h2.find_all('a'):
-#                     url_komik_popular = a.get('href')
-#                     title_komik_popular = a.text
-#                 for a in span:
-#                     link = a.find_all('a')
-#                     for title in link:
-#                         genre.append(title.text)
-#                 daftar_popular_manga.append(
-#                     {'image': src, "url": url_komik_popular, "title": title_komik_popular, 'genre': genre})
-
-#     return jsonify({
-#         'status': True,
-#         'response': daftar_popular_manga,
-#     })
-
-
 @app.route('/api/search', methods=['POST'])
 def search():
     query = request.form.get('query')
@@ -236,13 +124,22 @@ def search():
         a = link.find_all('a')
         for item in a:
             url = item.get('href')
-            class_title = item.find(class_='title')
-            img = item.find('img')
+            class_title = item.find_all('h3', class_='title')
+            class_img = item.find_all('img')
+            class_numscore = item.find_all(class_='numscore')
+            class_chapter = item.find_all(class_='chapter')
 
-            title = class_title.text
-            image = img['src']
+            for h3_title in class_title:
+                title = h3_title.text
+            for img in class_img:
+                image = img['src']
+            for numscore in class_numscore:
+                rating = numscore.text
+            for h3_title in class_chapter:
+                chapter = h3_title.text
 
-            daftar_komik.append({'url': url, 'title': title, 'image': image})
+            daftar_komik.append(
+                {'url': url, 'title': title, 'image': image, 'rating': rating, 'chapter': chapter})
 
     return jsonify({
         'status': True,
@@ -250,27 +147,39 @@ def search():
     })
 
 
-@app.route('/api/detail', methods=['POST'])
+@app.route('/api/komik', methods=['POST'])
 def detail():
-    url = request.form.get('url')
-    detail_page = req.get(url)
+    try:
+        url = request.form.get('url')
+        detail_page = req.get(url)
 
-    soup = BeautifulSoup(detail_page.text, 'html.parser')
+        soup = BeautifulSoup(detail_page.text, 'html.parser')
 
-    chapter_wrapper = soup.find_all('ul', id="chapter-wrapper")
+        chapter_wrapper = soup.find_all('ul', id="chapter-wrapper")
 
-    daftar_chapter = []
+        daftar_chapter = []
 
-    for chapter in chapter_wrapper:
-        for a in chapter.find_all('a'):
-            title = a.text
-            url = a.get('href')
-            daftar_chapter.append({'url': url, 'title': title})
+        for chapter in chapter_wrapper:
+            updated_at = []
+            for div in chapter.find_all('div', class_="chapter-link-time"):
+                updated_at.append(div.text)
+            updated_at_index = 0
+            for a in chapter.find_all('a'):
+                title = a.text
+                url = a.get('href')
+                daftar_chapter.append(
+                    {'url': url, 'title': title, 'updated_at': updated_at[updated_at_index]})
+                updated_at_index += 1
 
-    return jsonify({
-        'status': True,
-        'response': daftar_chapter,
-    })
+        return jsonify({
+            'status': True,
+            'response': daftar_chapter,
+        })
+    except NameError as e:
+        return jsonify({
+            'status': False,
+            'response': e
+        })
 
 
 @app.route('/api/chapter', methods=['POST'])
