927c4914a4d47115d3a94467043d345c770ecc0c
muhajir-artapuri <muhajir@artapuri.co.id>
2022-12-02 09:38:37 +0700

    feat: detail komik

--- frontend/src/pages/DetailKomikPage.jsx
+++ frontend/src/pages/DetailKomikPage.jsx
@@ -3,7 +3,13 @@ import { useNavigate } from "react-router-dom";
 import { postDetailKomik } from "../sources/api";
 
 const DetailKomikPage = () => {
-  const [daftarChapter, setDaftarChapter] = useState([]);
+  const [daftarChapter, setDaftarChapter] = useState({
+    title: "",
+    info_komik: [],
+    image: "",
+    daftar_chapter: [],
+    genre: [],
+  });
   const [isLoading, setIsLoading] = useState(false);
   const komikUrl = process.env.REACT_APP_BASE_KOMIK_URL;
 
@@ -41,6 +47,7 @@ const DetailKomikPage = () => {
     postDetailKomik(formData)
       .then((response) => {
         setDaftarChapter(response.data.response);
+        console.log(response.data.response);
         setIsLoading(false);
       })
       .catch((error) => {
@@ -76,10 +83,37 @@ const DetailKomikPage = () => {
         />
       </div>
       <div className="lg:mx-32 p-4 shadow-lg rounded-lg">
+        <div className="flex flex-wrap mb-4">
+          <div className="w-96 pb-4 pr-4">
+            <img src={daftarChapter.image} alt="thubnail" />
+          </div>
+          <div className="w-72 ">
+            <p className="font-semibold text-lg">{daftarChapter.title}</p>
+            <div className="flex flex-wrap sm:grid-cols-2 gap-2 mt-2 mb-6">
+              {daftarChapter.genre.map((genre) => {
+                return (
+                  <div className="rounded bg-sky-600 text-white px-2 py-1 h-max w-max">
+                    <p className="text-xs">{genre}</p>
+                  </div>
+                );
+              })}
+            </div>
+            {daftarChapter.info_komik.map((info) => {
+              return (
+                <p className="text-sm text-gray-400">
+                  {info.split(":")[0]}:
+                  <span className="text-sky-500 font-semibold">
+                    {info.split(":")[1]}
+                  </span>
+                </p>
+              );
+            })}
+          </div>
+        </div>
         <div className="border-b mb-4 py-2 text-sky-500">
           <p>Chapter</p>
         </div>
-        {daftarChapter.map((response) => {
+        {daftarChapter.daftar_chapter.map((response) => {
           return (
             <div
               className="cursor-pointer flex space-x-2 text-xs w-full justify-between border-b mb-2 py-2 font-semibold"
